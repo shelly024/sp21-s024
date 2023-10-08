15787bb22b4a2dbe35155ee079e61ac15759e472
shelly024 <z764365149@outlook.com>
2023-10-08 21:30:57 +0100

    done with Collatz

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,87 @@
+package deque;
+public class ArrayDeque<T> {
+    private T[] items;
+    private int size;
+    private int i;
+    private int nextfirst;
+    private int nextlast;
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        size = 0;
+        i = 3;
+        nextfirst = i - 1;
+        nextlast = i + 1;
+    }
+    public void addFirst(T item) {
+        items[nextfirst] = item;
+        if (nextfirst == 0) {
+            nextfirst = size - 1;
+        }else {
+            nextfirst = nextfirst - 1;
+        }
+        size = size + 1;
+    }
+
+    public void addLast(T item) {
+        items[nextlast] = item;
+        if (nextlast == size - 1) {
+            nextlast = 0;
+        }else {
+            nextlast = nextlast + 1;
+        }
+        size = size + 1;
+    }
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public int size() {
+        return size;
+    }
+    public void printDeque() {
+        for(int i = 0;i < size;i+=1){
+            System.out.print(items[i] + " ");
+        }
+    }
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        if (nextfirst == size) {
+            T a = items[0];
+            items[0] = null;
+            nextfirst = 0;
+            size = size - 1;
+            return a;
+        }
+        T a = items[nextfirst+1];
+        items[nextfirst+1] = null;
+        size = size - 1;
+        return a;
+    }
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        if (nextlast == 0) {
+            T b = items[size-1];
+            items[size-1] = null;
+            nextlast = size - 1;
+            size = size - 1;
+            return b;
+        }
+        T b = items[nextlast - 1];
+        items[nextlast - 1] = null;
+        size = size - 1;
+        return b;
+    }
+    public T get(int index) {
+        if (index > size - 1 || index < 0) {
+            return null;
+        }
+        return items[index];
+    }
+}
---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,92 @@
+package deque;
+
+import java.util.LinkedList;
+
+public class LinkedListDeque<T> {
+    private class IntNode {
+        public T item;
+        public IntNode prev;
+        public IntNode next;
+        public IntNode(IntNode p,T i,IntNode n){
+            prev = p;
+            item = i;
+            next = n;
+        }
+    }
+    private IntNode sentinel;
+    private int size;
+    public LinkedListDeque() {
+        sentinel=new IntNode(null,null,null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size=0;
+    }
+    public LinkedListDeque(T x) {
+        sentinel = new IntNode(null,null,null);
+        sentinel.next = new IntNode(sentinel,x,sentinel);
+        sentinel.prev = sentinel.next;
+        size=1;
+    }
+    public void addFirst(T item) {
+        sentinel.next = new IntNode(sentinel,item,sentinel.next);
+        sentinel.prev = sentinel.next;
+        size = size + 1;
+    }
+    public void addLast(T item) {
+        IntNode s = sentinel.prev;
+        sentinel.prev = new IntNode(sentinel.prev,item,sentinel);
+        s.next = sentinel.prev;
+        size = size + 1;
+    }
+    public boolean isEmpty() {
+        if(size == 0) {
+            return true;
+        }
+        return false;
+    }
+    public int size() {
+        return size;
+    }
+    public void printDeque() {
+        for(int i=0;i<=size-1;i+=1){
+            System.out.print(get(i) + " ");
+        }
+    }
+    public T removeFirst() {
+        if (size == 0){
+            return null;
+        }
+        IntNode x = sentinel.next;
+        T a = sentinel.next.item;
+        IntNode p = sentinel.next.next;
+        sentinel.next = p;
+        sentinel.next.prev = sentinel;
+        size = size - 1;
+        x.next = null;x.prev = null;
+        return a;
+    }
+    public T removeLast() {
+        if (size == 0){
+            return null;
+        }
+        IntNode x = sentinel.prev;
+        IntNode p = sentinel.prev.prev;
+        T a = sentinel.prev.item;
+        sentinel.prev = p;
+        sentinel.prev.next = sentinel;
+        size = size - 1;
+        x.next = null;x.prev = null;
+        return a;
+    }
+    public T get(int index) {
+        IntNode p = sentinel;
+        if (index < size){
+            for(int i = 0;i <= index;i+=1){
+                p = p.next;
+            }
+        }else {
+            return null;
+        }
+        return p.item;
+    }
+}
